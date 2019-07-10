30bad46d58502600b87822e5de00d0687a2a0d82
sejalpradhan <52529655+sejalpradhan@users.noreply.github.com>
2019-07-10 17:16:46 +0530

    Add files via upload

---
+++ Untitled (1).py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Markdown Basics
+# ## Markdown Basics
+# ### Markdown Basics
+
+# **point 1**
+# 
+# *point 1*
+# 
+# ***point1***
+
+# ### Python Basics
+# * Python version 3.7
+# - Functional Programming
+# - Object oriented Programming
+# - Scripting Programming
+
+# In[2]:
+
+
+print ("hello")
+
+
+# In[3]:
+
+
+print ("GITAM,")
+print ("Hyderabad")
+
+
+# In[6]:
+
+
+print ("Hello, GITAM","|||",end ="")
+print ("Hyderabad","|||",end ="")
+print ("Python Programming")
+
+
+# In[7]:
+
+
+n1=100
+print (n1)
+
+
+# In[8]:
+
+
+n1=100
+a=b=c=20
+print (n1)
+
+
+# In[ ]:
+
+
+# test given number is positive
+
